96d923b0880652b77609baa6ed6e0b1a57095100
Mike Files <m.kefiles@gmail.com>
2025-11-10 13:03:37 -0500

    Spring Project added for Kafka Consumer

---
+++ src/main/java/net/yorksolutions/tsg_9/__TheAvengers_feedback_analytics_consumer/Application.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.tsg_9.__TheAvengers_feedback_analytics_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/yorksolutions/tsg_9/__TheAvengers_feedback_analytics_consumer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.tsg_9.__TheAvengers_feedback_analytics_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
